b5563b6334e66ef235a03b902547fe6f05e537d5
Adam Woodman <adamwoodman@gmail.com>
2015-09-03 11:03:52 -0600

    I changed the Point.cpp, Point.h, and README.md files to work with three points. The program now takes in x, y, and z coordinates from the user for three points and calculates the area of the traingle incribed in points 1, 2, and 3 in space. The area is returned to the user at the end of the program.

--- Point.cpp
+++ Point.cpp
@@ -5,20 +5,24 @@
 Point::Point() {
   x = 0.0;
   y = 0.0;
+  z = 0.0;
 }
 
-// Constructor
+// Custom Constructor
 // Initializes the point to (initX, initY)
-Point::Point(double initX, double initY) {
+Point::Point(double initX, double initY, double initZ) {
   x = initX;
   y = initY;
+  z = initZ;
 }
+
 
 // Destructor
 // No dynamic allocation, so nothing to do; if omitted, generated automatically
 Point::~Point() {
   // no-op
 }
+
 
 // Mutator methods
 // Change the values of private member variables
@@ -31,6 +35,11 @@ void Point::setY(double newY) {
   y = newY;
 }
 
+void Point::setZ(double newZ) {
+  z = newZ;
+}
+
+
 // Accessors
 // Return the current values of private member variables
 
@@ -40,4 +49,17 @@ double Point::getX() {
 
 double Point::getY() {
   return y;
+}
+
+double Point::getZ() {
+  return z;
+}
+
+
+// Distance Calculator Function
+
+double Point::distanceTo(Point &p) {
+  double dist;
+  dist = sqrt(pow((p.getX()-x),2)+pow((p.getY()-y),2)+pow((p.getZ()-z),2));      // sqrt((x2-x1)^2+(y2-y1)^2+(z2-z1)^2)
+  return dist;
 }
--- Point.h
+++ Point.h
@@ -1,19 +1,24 @@
-// A 2-dimensional point class!
+// A 3-dimensional point class!
 // Coordinates are double-precision floating point.
 
 #ifndef __point_h
 #define __point_h
+
+#include <cmath>
+#include <iostream>
+#include <iomanip>
 
 class Point {
 
 private:
   double x;
   double y;
+  double z;
 
 public:
   // Constructors
-  Point();                      // default constructor
-  Point(double x, double y);    // two-argument constructor
+  Point();                                   // default constructor
+  Point(double x, double y, double z);       // three-argument constructor
 
   // Destructor
   ~Point();
@@ -21,10 +26,15 @@ public:
   // Mutator methods
   void setX(double newX);
   void setY(double newY);
+  void setZ(double newZ);
 
   // Accessor methods
   double getX();
   double getY();
+  double getZ();
+
+  // distance function
+  double distanceTo(Point &p);      // takes in a reference to a point
 };
 
 #endif // __point_h
